d562a0d08eb707706fa5cd474c7826eb8535729f
Adeel <adeel@Adeels-Air.fritz.box>
2026-02-19 23:14:28 +0100

    chore: remove tmp-test artifacts and ignore local test output

--- tmp-test/src/shared/utils/startupRoute.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getStartupRoute = void 0;
-const getStartupRoute = (hasUser) => hasUser ? '(tabs)/my-shifts' : '/startup';
-exports.getStartupRoute = getStartupRoute;
